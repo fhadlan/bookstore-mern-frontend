69bb1a086f14653c8a9d5e949255c7580865fbf9
Fadlan Fadilah <fhadlan123@gmail.com>
2025-01-28 08:44:58 +0700

    setting up tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router";
 function App() {
   return (
     <>
-      <nav>Header</nav>
-      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="font-primary">Header</nav>
+      <main className="font-secondary mx-auto min-h-screen max-w-screen-2xl px-4 py-6">
         <Outlet />
       </main>
       <footer>Footer</footer>
