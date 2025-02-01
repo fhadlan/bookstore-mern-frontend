81b9df8825601b9e38224ca54ee41917a4132007
Fadlan Fadilah <fhadlan123@gmail.com>
2025-02-01 14:48:17 +0700

    setting up single book interface

--- src/pages/books/SingleBook.jsx
+++ src/pages/books/SingleBook.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { Link, useParams } from "react-router";
+import { useFetchSingleBookQuery } from "../../redux/features/book/bookApi";
 const SingleBook = () => {
-  return <div>SingleBook</div>;
+  const { id } = useParams();
+  const { data: book, isLoading, isError } = useFetchSingleBookQuery(id);
+  console.log(book);
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error</div>}
+      SingleBook
+    </div>
+  );
 };
 
 export default SingleBook;
