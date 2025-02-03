3d9692185f9156a74cdae37b374b11b44426c4a1
Fadlan Fadilah <fhadlan123@gmail.com>
2025-02-03 09:10:02 +0700

    buat auth provider using firebase

---
+++ src/context/AuthCOntext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+//auth provider
+export const AuthProvider = ({ children }) => {
+  const value = {};
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
