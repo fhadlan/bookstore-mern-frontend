c4b588ba03212da2d949d1403b9b9123f0db517f
Fadlan Fadilah <fhadlan123@gmail.com>
2025-02-03 16:17:17 +0700

    bug fix protected route

--- src/routers/LoginRegister.jsx
+++ src/routers/LoginRegister.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router";
-import { useAuth } from "../context/AuthContext";
+import { useNavigate, Outlet } from "react-router";
+import { auth } from "../firebase/firebase.config";
 
 function LoginRegister() {
-  const { currentUser } = useAuth();
+  const [loading, setLoading] = React.useState(true);
+  const navigate = useNavigate();
 
-  if (currentUser) {
-    return <Navigate to="/" />;
+  React.useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate("/");
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, [navigate]);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-32 w-32 animate-spin rounded-full border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
   return <Outlet />;
 }
--- src/routers/PrivateRoute.jsx
+++ src/routers/PrivateRoute.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import { useAuth } from "../context/AuthContext";
-import { Navigate, Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { auth } from "../firebase/firebase.config";
+import { Outlet, useNavigate } from "react-router";
 
 function PrivateRoute() {
-  const { currentUser } = useAuth();
+  const [loading, setLoading] = React.useState(true);
+  const navigate = useNavigate();
 
-  if (!currentUser) {
-    return <Navigate to="/login" />;
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        navigate("/login");
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, [navigate]);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-32 w-32 animate-spin rounded-full border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
   return <Outlet />;
 }
