5e9b2fdf4a83176fed7beedb5bf278595535d6dc
Fadlan Fadilah <fhadlan123@gmail.com>
2025-02-03 09:15:28 +0700

    auth context file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <main className="font-secondary mx-auto min-h-screen max-w-screen-xl px-4 py-6">
-        <Outlet />
-      </main>
-      <Footer />
+      <AuthProvider>
+        <Navbar />
+        <main className="font-secondary mx-auto min-h-screen max-w-screen-xl px-4 py-6">
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
