88e4d2e2143d09bb04fbe2540ddfcd4de32810c9
Fadlan Fadilah <fhadlan123@gmail.com>
2025-04-21 13:58:21 +0700

    single book page quantity, cart slice items quantity

--- src/pages/books/BookCard.jsx
+++ src/pages/books/BookCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
 import { FaShoppingCart } from "react-icons/fa";
-import { getImgUrl } from "../../utils/getImgUrl";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/features/cart/cartSlice";
 
@@ -14,7 +13,7 @@ const BookCard = ({ book }) => {
 
   return (
     <div className="rounded-lg transition-shadow duration-300">
-      <div className="flex flex-col gap-4 sm:h-72 sm:flex-row sm:items-center sm:justify-center">
+      <div className="flex flex-col gap-4 sm:h-72 sm:flex-row sm:items-center">
         <div className="rounded-md shadow-sm sm:h-72 sm:flex-shrink-0">
           <Link to={`/book/${book._id}`}>
             <img
@@ -25,44 +24,50 @@ const BookCard = ({ book }) => {
           </Link>
         </div>
 
-        <div>
-          <Link to={`/book/${book._id}`}>
-            <h3 className="font-primary mb-3 text-lg font-semibold hover:text-blue-600">
-              {book.title}
-            </h3>
-          </Link>
-          <p className="font-secondary mb-5 text-sm text-gray-600">
-            {book.description.length > 80
-              ? `${book.description.slice(0, 80)}...`
-              : book.description}
-          </p>
-          <p className="mb-5 font-medium">
-            {book.discountedPrice.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}{" "}
-            {book.discountedPrice !== book.price && (
-              <span className="ml-2 font-normal line-through">
-                {book.price.toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                })}
-              </span>
-            )}
-          </p>
-          <p
-            className={`mb-2 text-sm ${book.quantity > 0 ? "text-gray-500" : "text-red-600"}`}
-          >
-            {book.quantity > 0 ? `In Stock: ${book.quantity}` : "Out of Stock"}
-          </p>
-          <button
-            onClick={() => handleAddToCart(book)}
-            className="bg-primary hover:bg-secondary flex items-center gap-1 space-x-1 rounded-md px-3 py-1 shadow-sm transition-all duration-200 hover:cursor-pointer hover:text-white disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-white"
-            disabled={book.quantity === 0}
-          >
-            <FaShoppingCart className="" />
-            <span>Add to Cart</span>
-          </button>
+        <div className="flex h-full flex-col">
+          <div className="flex flex-grow flex-col">
+            <Link to={`/book/${book._id}`}>
+              <h3 className="font-primary mb-3 text-lg font-semibold hover:text-blue-600">
+                {book.title}
+              </h3>
+            </Link>
+            <p className="font-secondary mb-5 text-sm text-gray-600">
+              {book.description.length > 80
+                ? `${book.description.slice(0, 80)}...`
+                : book.description}
+            </p>
+            <p className="mb-5 font-medium">
+              {book.discountedPrice.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}{" "}
+              {book.discountedPrice !== book.price && (
+                <span className="ml-2 font-normal line-through">
+                  {book.price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </span>
+              )}
+            </p>
+          </div>
+          <div className="flex flex-col items-end justify-end">
+            <p
+              className={`mb-2 text-sm ${book.quantity > 0 ? "text-gray-500" : "text-red-600"}`}
+            >
+              {book.quantity > 0
+                ? `In Stock: ${book.quantity}`
+                : "Out of Stock"}
+            </p>
+            <button
+              onClick={() => handleAddToCart(book)}
+              className="bg-primary hover:bg-secondary ml-2 flex items-center justify-center gap-1 space-x-1 rounded-md px-3 py-1 shadow-sm transition-all duration-200 hover:cursor-pointer hover:text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500"
+              disabled={book.quantity === 0}
+            >
+              <FaShoppingCart />
+              <span>Add to Cart</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/books/SingleBook.jsx
+++ src/pages/books/SingleBook.jsx
@@ -9,6 +9,7 @@ import { FaShoppingCart } from "react-icons/fa";
 const SingleBook = () => {
   const { id } = useParams();
   const { data: book, isLoading, isError } = useFetchSingleBookQuery(id);
+  const [quantity, setQuantity] = React.useState(1);
   //console.log(book);
 
   const dispatch = useDispatch();
@@ -17,8 +18,12 @@ const SingleBook = () => {
     dispatch(addToCart(product));
   };
 
-  isLoading && <div>Loading...</div>;
-  isError && <div>Error</div>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error</div>;
+  }
 
   return (
     <div>
@@ -26,46 +31,82 @@ const SingleBook = () => {
         <h3 className="font-primary mb-6 text-xl font-semibold">
           {book?.title}
         </h3>
-        <div className="flex gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row">
           <div className="flex flex-shrink-0 flex-col items-center">
             <img
               src={book?.coverImage}
               alt=""
               className="cursor-pointer rounded-md bg-cover"
             />
-            <button
-              onClick={() => handleAddToCart(book)}
-              className="bg-primary hover:bg-secondary mt-2 flex w-full items-center justify-center gap-1 space-x-1 rounded-md px-3 py-1 shadow-sm transition-all duration-200 hover:cursor-pointer hover:text-white"
-            >
-              <FaShoppingCart />
-              <span>Add to Cart</span>
-            </button>
           </div>
-          <div className="flex flex-col gap-3">
-            <span>
-              <strong>Author:</strong> {book?.author}
-            </span>
-            <span>
-              <strong>Category:</strong> {book?.category}
-            </span>
-            <span>
-              <strong>Description:</strong> {book?.description}
-            </span>
-            <span>
-              <strong>Price:</strong>{" "}
-              {book?.discountedPrice.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}{" "}
-              {book?.discountedPrice !== book?.price && (
-                <span className="line-through">
-                  {book?.price.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  })}
-                </span>
-              )}
-            </span>
+          <div className="flex w-full flex-col">
+            <div className="flex flex-grow flex-col gap-3">
+              <span>
+                <strong>Author:</strong> {book?.author}
+              </span>
+              <span>
+                <strong>Category:</strong> {book?.category}
+              </span>
+              <span>
+                <strong>Description:</strong> {book?.description}
+              </span>
+              <span>
+                <strong>Price:</strong>{" "}
+                {book?.discountedPrice.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}{" "}
+                {book?.discountedPrice !== book?.price && (
+                  <span className="line-through">
+                    {book?.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </span>
+                )}
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <div className="flex flex-grow items-end justify-end">
+                <p
+                  className={`text-md ${book?.quantity > 0 ? "text-gray-500" : "text-red-600"}`}
+                >
+                  {book?.quantity > 0
+                    ? `In Stock: ${book.quantity}`
+                    : "Out of Stock"}
+                </p>
+              </div>
+              <div className="flex flex-row justify-end">
+                <label className="px-2 py-1">
+                  Quantity:
+                  <select
+                    className="ml-1 rounded-md px-2 py-1"
+                    name="quantity"
+                    onChange={(e) => setQuantity(e.target.value)}
+                  >
+                    {[...Array(book?.quantity).keys()].map((i) => (
+                      <option key={i + 1} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                  </select>
+                </label>
+
+                <button
+                  onClick={() =>
+                    handleAddToCart({
+                      _id: book?._id,
+                      quantity,
+                    })
+                  }
+                  className="bg-primary hover:bg-secondary flex items-center justify-center gap-1 space-x-1 rounded-md px-3 py-1 shadow-sm transition-all duration-200 hover:cursor-pointer hover:text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500"
+                  disabled={book?.quantity === 0}
+                >
+                  <FaShoppingCart />
+                  <span>Add to Cart</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
